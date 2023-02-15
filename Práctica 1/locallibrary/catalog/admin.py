4b1ccecaebbdb399ab2779df06c1faf566d02f6d
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-02-15 15:46:23 +0100

    Semana 2 terminada

--- Práctica 1/locallibrary/catalog/admin.py
+++ Práctica 1/locallibrary/catalog/admin.py
@@ -10,6 +10,7 @@ from django.contrib import admin
 
 class BooksInline(admin.TabularInline):
     model = Book
+    extra = 0
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
@@ -22,6 +23,7 @@ admin.site.register(Author, AuthorAdmin)
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+    extra = 0
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
--- Práctica 1/locallibrary/catalog/views.py
+++ Práctica 1/locallibrary/catalog/views.py
@@ -14,12 +14,15 @@ def index(request):
     
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.count()
     
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
     }
     
     # Render the HTML template index.html with the data in the context variable
@@ -29,20 +32,17 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
+    queryset = Book.objects.order_by('title')
 
-    paginate_by = 10
+    paginate_by = 2
     
 class BookDetailView(generic.DetailView):
     model = Book
     
-    paginate_by = 10
-    
 class AuthorListView(generic.ListView):
     model = Author
-    
+
     paginate_by = 10
     
 class AuthorDetailView(generic.DetailView):
     model = Author
-    
-    paginate_by = 10
