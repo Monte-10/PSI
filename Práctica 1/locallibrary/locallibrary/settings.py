0fde9e008ae1c53134abf2d2c71fb397f1435f86
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-02-16 14:16:39 +0100

    En mitad de una cosa

--- Práctica 1/locallibrary/locallibrary/settings.py
+++ Práctica 1/locallibrary/locallibrary/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
